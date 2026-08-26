a8e257305e038ef6396730078dc739eca26ebcc3
Carlos Alberto Gomez Murillo <carlosgomezmurillo15@gmail.com>
2026-08-25 19:14:07 -0500

    Add theme toggle functionality and current year display

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const themeButton = document.querySelector("#theme-button");
+const currentYear = document.querySelector("#current-year");
 
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle("light-theme");
+
+  const lightThemeIsActive =
+    document.body.classList.contains("light-theme");
+
+  themeButton.textContent =
+    lightThemeIsActive ? "Dark mode" : "Light mode";
+});
+
+currentYear.textContent = new Date().getFullYear();
